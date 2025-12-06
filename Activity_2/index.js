9a051879ea34eb9795daf5bb4fc21a612733df12
Uno Tarun <unotarun01@gmail.com>
2025-12-06 10:17:52 +0800

    commit

---
+++ Activity_2/index.js
@@ -0,0 +1,10 @@
+function validate() {
+
+    let email = document.getElementById("email").value;
+
+if (email === "") {
+    alert("Email is required.");
+} else {
+        alert("Form submitted.");
+    }
+}
